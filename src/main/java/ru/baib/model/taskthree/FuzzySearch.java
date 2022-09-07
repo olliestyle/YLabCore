b9aef4bbff3b03437a823bf6dd79835c4298368b
olliestyle <olliestyle1989@gmail.com>
2022-09-07 15:34:18 +0300

    Add output result

--- src/main/java/ru/baib/model/taskthree/FuzzySearch.java
+++ src/main/java/ru/baib/model/taskthree/FuzzySearch.java
@@ -1,6 +1,23 @@
 package ru.baib.model.taskthree;
 
 public class FuzzySearch {
+
+    public static void main(String[] args) {
+        FuzzySearch fuzzySearcher = new FuzzySearch();
+        fuzzySearcher.outputResult("car", "ca6$$#_rtwheel", fuzzySearcher);
+        fuzzySearcher.outputResult("cwhl", "cartwheel", fuzzySearcher);
+        fuzzySearcher.outputResult("cwhee", "cartwheel", fuzzySearcher);
+        fuzzySearcher.outputResult("cartwheel", "cartwheel", fuzzySearcher);
+        fuzzySearcher.outputResult("cwheeel", "cartwheel", fuzzySearcher);
+        fuzzySearcher.outputResult("lw", "cartwheel", fuzzySearcher);
+        fuzzySearcher.outputResult("asd", "", fuzzySearcher);
+    }
+
+    public void outputResult(String detect, String original, FuzzySearch fuzzySearcher) {
+        System.out.println("Is string \"" + detect + "\"" + " is fuzzy found in string \"" + original + "\": "
+                + fuzzySearcher.fuzzySearch(detect, original));
+    }
+
     public boolean fuzzySearch(String detect, String original) {
         int detectLength = detect.length();
         int originalLength = original.length();
